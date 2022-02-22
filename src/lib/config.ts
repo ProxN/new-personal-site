fddbb5ee86cbef0e0740688806ba31e00f587650
ProxN <idel.ayoubkan@gmail.com>
2022-02-22 02:19:12 +0100

    edit site title

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,8 @@
 export const config = {
-  siteTitle: 'Ayoub Idelkanoun | Front-end Developer',
-  siteDescription: 'Ayoub Idelkanoun is a front-end developer from Morocco.',
+  siteTitle: 'Ayoub Idelkanoun | Full-Stack Developer',
+  siteDescription: 'Ayoub Idelkanoun is a Full-Stack developer from Morocco.',
   keywords:
-    'Ayoub Idelkanoun,front-end developer,web developer,morocoo web web developer,javascript',
+    'Ayoub Idelkanoun,full-stack developer,web developer,morocoo web developer,javascript',
   googleVerification: '88CPTnKcSQeWAFbAbgXWDVUR-38ZzTmT6QPnd2i-7zo',
   siteUrl: 'https://www.ayoubkanoun.com/',
 };
